017b418500daf0862cb962a8b905ebb035718f25
navyzal <navyzal@gmail.com>
2022-03-29 10:43:15 +0900

    mouse control -> keyboard control

--- HandTracking.py
+++ HandTracking.py
@@ -1,9 +1,12 @@
 import socket
+import time
 import pyautogui
+#import pydirectinput
 import math
 import cv2
 import mediapipe as mp
 import numpy as np
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
@@ -101,12 +104,16 @@ with mp_hands.Hands(
     # To improve performance, optionally mark the image as not writeable to
     # pass by reference.
     image.flags.writeable = False
+    image = image.copy()
+    start = time.time()
+    image = cv2.flip(image, 1)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = hands.process(image)
     
-    judgeImg = np.zeros((512,512,3), np.uint8)
+    #judgeImg = np.zeros((512,512,3), np.uint8)
     leftHandIdx = -1
     rightHandIdx = -1
+    
     
     # Draw the hand annotations on the image.
     image.flags.writeable = True
@@ -127,7 +134,7 @@ with mp_hands.Hands(
           if handedness == 'Right':
             
             leftFingerSign = getFingerSign(hand_landmarks.landmark, False)            
-            cv2.putText(judgeImg, "Left finger gesture: "  + str(leftFingerSign) , (20, 300), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255))
+            cv2.putText(image, "Left finger gesture: "  + str(leftFingerSign) , (20, 300), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
             
             '''
             print('left handedness')            
@@ -140,7 +147,7 @@ with mp_hands.Hands(
           ## 사실은... 오른손임.. 
           elif handedness == 'Left':            
             rightFingerSign = getFingerSign(hand_landmarks.landmark)            
-            cv2.putText(judgeImg, "Right finger gesture: "  + str(rightFingerSign) , (20, 400), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255))
+            cv2.putText(image, "Right finger gesture: "  + str(rightFingerSign) , (20, 400), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
             
             '''
             print('right handedness')            
@@ -152,20 +159,18 @@ with mp_hands.Hands(
             
           else:
             cv2.putText(image, "finger gesture: lost", (20, 300), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+        
+          
 
       '''
       else:
-        cv2.putText(image, "finger gesture: lost", (20, 300), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)  
+        cv2.putText(image, "finger gesture: lost", (20, 300), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)   
         '''
-    
-    
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('Judgement', judgeImg)
-    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-    #cv2.imshow('MediaPipe Hands', image)
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
-    
+    end = time.time()
+    totalTime = end - start
+    fps = 1 / totalTime
+    cv2.putText(image, f'FPS: {int(fps)}', (20,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+ 
     
     if leftFingerSign == 'Focus' and rightFingerSign == 'Focus':
       print("FOCUS MODE ON")
@@ -184,30 +189,37 @@ with mp_hands.Hands(
           LeftHandSize = 1
           RightHandSize = 1
       handRatio = 0
-      if LeftHandSize > RightHandSize:
-        handRatio = -(LeftHandSize / RightHandSize)
-      else:
-        handRatio = RightHandSize / LeftHandSize
       
-      if -1.1 < handRatio < 1.1:
-        handRatio = 0
-      print(LeftHandSize, RightHandSize, handRatio)
-      mouseMoveLength = 10
+      direction = 0   # -1 왼쪽 0 중간 +1 오른쪽
+      if LeftHandSize > RightHandSize: 
+        handRatio = LeftHandSize * 10 / RightHandSize
+        direction = 1
+      elif LeftHandSize < RightHandSize:
+        handRatio = RightHandSize * 10 / LeftHandSize
+        direction = -1
+      else:         
+        direction = 0
       
-      bytesToSend = str.encode(str(mouseMoveLength * handRatio))
-      serverAddressPort = ("192.168.0.171", 20001) 
-      bufferSize = 1024 
-
-
-      # 클라이언트 쪽에서 UDP 소켓 생성 
-      UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM) 
-
-      # 생성된 UDP 소켓을 사용하여 서버로 전송 
-
-      UDPClientSocket.sendto(bytesToSend, serverAddressPort) 
+      if not (RightHandSize == 0 or LeftHandSize == 0):
+        if handRatio > 20:
+          handRatio = 10
+        else:
+          handRatio = math.trunc(handRatio) - 10
+          
+      cv2.putText(image, f'rotation: {handRatio*direction}', (20,100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
             
-      # Todo: 가속 개념 추가 고려
-      pyautogui.moveRel(mouseMoveLength * handRatio, 0)
-      
+      if direction == -1:
+        pyautogui.press('left', presses=handRatio)
+      elif direction == 1:
+        pyautogui.press('right', presses=handRatio)
+   
+    # Flip the image horizontally for a selfie-view display.
+    #cv2.imshow('Judgement', judgeImg)
+    #cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+    cv2.imshow('MediaPipe Hands', image)
+        
+    if cv2.waitKey(5) & 0xFF == 27:
+      break
+    
 cap.release()
 cv2.destroyAllWindows()
